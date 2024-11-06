d555f853d01624fd666188d76e666ea62a41cc70
joselisboaa <joselisboaa05@gmail.com>
2024-11-05 23:35:21 -0300

    feat

--- map-acess-backend-development/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    YELLOW_THRESHOLD: 20,
-    RED_THRESHOLD: 30,
-    CORS: process.env.FRONTEND_URL || '*',
-    PORT: 8000
-}
--- map-acess-backend-development/src/middlewares/exceptions/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export const errorHandler = function (err, req, res, next) {
-   
-    res.status(err.statusCode).send({"message": err.message, "cause": err.cause})
-}
