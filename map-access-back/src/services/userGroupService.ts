b96bed33db101547bb6e1beb0dccf9a12adeca2f
joselisboaa <joselisboaa05@gmail.com>
2025-02-06 20:15:25 -0300

    refactor: add facade in userGroup

--- map-access-back/src/services/userGroupService.ts
+++ map-access-back/src/services/userGroupService.ts
@@ -1,74 +1,32 @@
-import { PrismaClient, UserGroup } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { UserGroupRepository } from '../repository/userGroupRepository';
 
 export class UserGroupService {
-  async verifyEntityDependencies(req, res): Promise<UserGroup | null> {
+  private repository = new UserGroupRepository();
+
+  async verifyEntityDependencies(req, res) {
     const userId = Number(req.params["id"]);
-
-    const query = {
-      where: { id: userId },
-      include: { users: true, questions: true }
-    }
-
-    return prisma.userGroup.findFirst(query);
+    return this.repository.verifyEntityDependencies(userId);
   }
 
   async findAll(req, res) {
-    const queryParam = req.query
-
-    let filter = {
-        text: {
-            startsWith: queryParam["text"]
-        }
-    }
-
-    const query = {
-        skip: queryParam["pg"] == null? 0 : ( Number(queryParam["qt"]) * (Number(queryParam["pg"]) - 1) ),
-        take: queryParam["qt"] == null? 100 : Number(queryParam["qt"]),
-        where: filter
-    }
-
-    return prisma.userGroup.findMany(query);
+    return this.repository.findAll(req.query);
   }
 
   async findById(id) {
-    return prisma.userGroup.findUnique({
-      where: {
-        id: id,
-      },
-    });
+    return this.repository.findById(id);
   }
 
   async create(data) {
-    return prisma.userGroup.create({
-      data: {
-        text: data["text"],
-      },
-    });
+    return this.repository.create(data);
   }
 
   async update(req, res) {
-    const data = {
-      "id": res.locals.userGroup["id"],
-      "text": req.body["text"]
-    }
-    
-    return prisma.userGroup.update({
-      where: {
-        id: data["id"],
-      },
-      data: {
-        text: data["text"],
-      },
-    });
+    const id = res.locals.userGroup["id"];
+    const text = req.body["text"];
+    return this.repository.update(id, text);
   }
 
   async deleteById(data) {
-    return prisma.userGroup.delete({
-      where: {
-        id: data["id"],
-      },
-    });
+    return this.repository.deleteById(data["id"]);
   }
 }
